4327dbd752344df5a105dd34641d7cef6c712564
CarllosHenrique <crick.lucas@gmail.com>
2025-01-11 19:14:05 -0300

    fix: change to haml

--- db/schema.rb
+++ db/schema.rb
@@ -74,9 +74,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_183932) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username", null: false
-    t.string "email"
     t.string "password_digest"
     t.string "password"
+    t.datetime "last_active_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
